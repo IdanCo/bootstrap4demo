39c963799b773ca2385edf1523a19b84db682122
Idan Cohen <idan@idancohen.com>
2017-08-22 18:35:49 +0300

    responsive

--- src/components/main/main.component.js
+++ src/components/main/main.component.js
@@ -3,18 +3,14 @@ import MainTemplate from './main.html';
 
 // Set up controller
 class MainController {
-  constructor($element) {
-    this.text = 'Angular ' + angular.version.full + ' is working!';
-    this.$element = $element;
+
+  constructor() {
+
   }
 
   $onInit() {
-    this.$element.find('.test-jquery').text('jQuery ' + $.fn.jquery + ' is working!');
   }
 }
-
-// annotate injections
-MainController.$inject = ['$element'];
 
 // Define and export component
 export default {
